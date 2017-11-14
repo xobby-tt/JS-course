2c3a97a12cf34f2ad90f4c2fb3d468fdc2dcba86
xobby_tt <nastya-nastenkaa@mail.ru>
2017-11-14 16:00:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,107 @@
+var list = document.createElement("div");
+var addLi = document.createElement("div");
+
+addLi.classList.add("cell");
+addLi.classList.add("main");
+
+var plus = document.createElement("div");
+plus.classList.add("img-plus");
+var input = document.createElement("textarea");
+
+addLi.appendChild(input);
+addLi.appendChild(plus);
+
+document.body.appendChild(addLi);
+
+plus.onclick = function(event) {
+        if (input.value) {
+        var newDiv = document.createElement("div");
+        newDiv.classList.add("cell");
+        newDiv.innerHTML = input.value;
+        newDiv.setAttribute("mobile", "true");
+        list.appendChild(newDiv);
+        input.value = "";
+    }
+}
+
+document.body.appendChild(list);
+
+document.body.onmouseover = function(event) {
+    var target = event.target;    
+    if (!target.getAttribute("mobile"))
+    return;
+
+    target.style.background = "lightgrey";
+    var deleteElem = document.createElement("div");
+    deleteElem.classList.add = "delete";
+
+    target.appendChild(deleteElem);
+
+    deleteElem.onclick = function() {
+        target.parentNode.deleteElem(target);
+    }
+
+    target.onmouseout = function() {
+        target.style.background = "";
+        target.removeChild(deleteElem);
+    }
+}
+
+
+document.body.onmousedown = function(event) {
+    var target = event.target;
+
+    if (!target.getAttribute("mobile"))
+        return;
+
+    var coords = getCoords(target);
+    var shiftX = event.pageX - coords.left;
+    var shiftY = event.pageY - coords.top;
+
+    target.style.position = "absolute";
+    moveAt(event);
+    
+    document.onmousemove = function(e) {
+        moveAt(e);
+    }
+
+    target.onmouseup = function(e) {
+        document.onmousemove = null;
+        target.onmouseup = null;
+
+        var heightTarget = target.getBoundingClientRect().bottom - target.getBoundingClientRect().top;
+        
+                var sibling = document.elementFromPoint(20, target.getBoundingClientRect().bottom - heightTarget/2);
+                if(sibling.className == "cell" && sibling != target) {
+                    var  heightSibling = sibling.getBoundingClientRect().bottom - sibling.getBoundingClientRect().top;
+                    if (target.getBoundingClientRect().bottom - heightTarget/2 < sibling.getBoundingClientRect().bottom - heightSibling / 2)
+                        sibling.before(target);
+                }
+
+                if (target.getBoundingClientRect().top < list.getBoundingClientRect().top)
+                    list.insertBefore(target, list.firstElementChild);
+                if (target.getBoundingClientRect().top > list.getBoundingClientRect().bottom)
+                    list.appendChild(target);
+
+        target.style.position = "";
+    }
+
+    target.ondragstart = function() {
+        return false;
+    }
+
+    function moveAt(e) {
+        target.style.left = e.pageX - shiftX + 'px';
+        target.style.top = e.pageY - shiftY + 'px';
+    }
+
+    function getCoords(elem) {
+        var box = elem.getBoundingClientRect();
+
+        return {
+            top: box.top + pageYOffset,
+            left: box.left + pageXOffset,
+        }
+    }
+}
+
